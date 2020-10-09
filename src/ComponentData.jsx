c3cea25adee4eb472e66edd732f1708d1bdfe882
Beste Burcu Bayhan <bestebbayhan@gmail.com>
2020-10-09 20:28:30 +0300

    Add markers and pop-up to show the data

---
+++ src/ComponentData.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const ComponentData = (props) => {
+  return <h1>props.data.name</h1>;
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import mapboxgl from "mapbox-gl";
+import { ComponentData } from "./ComponentData";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiYmVzdGViYXloYW4iLCJhIjoiY2tibm00NDB4MXUyNDJ5bDlqZGZ1ZjNpNCJ9.FP-7Bm0LzNmmnfxgQfzFMw";
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+
+const mockedData = require("./mockFile.json");
 
 class Application extends React.Component {
   constructor(props) {
@@ -13,6 +17,7 @@ class Application extends React.Component {
       lng: 35.6412,
       lat: 40.0829,
       zoom: 5.3,
+      markers: [],
     };
   }
   componentDidMount() {
@@ -22,16 +27,20 @@ class Application extends React.Component {
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom,
     });
+
+    this.setState({
+      markers: mockedData.map((data) => {
+        return new mapboxgl.Marker(data)
+          .setLngLat(data.city)
+          .setPopup(new mapboxgl.Popup().setHTML(`<h1>${data.name}</h1>`))
+          .addTo(map);
+      }),
+    });
   }
+
   render() {
     return (
       <div>
-        <div className="sidebarStyle">
-          <div>
-            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
-            {this.state.zoom}
-          </div>
-        </div>
         <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
       </div>
     );
